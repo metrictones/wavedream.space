5e4d8ea461265a48013e22f8ec50a9e4679c1aa8
metrictones <64150105+metrictones@users.noreply.github.com>
2023-04-29 23:31:51 -0700

    2 files modified, 1 added

---
+++ src/routes/svelvet/+page.svelte
@@ -0,0 +1,10 @@
+<script>
+  import { Node, Svelvet, Minimap, Controls } from 'svelvet';
+</script>
+
+<div id="fixed" class="h-screen">
+<Svelvet theme="dark" snapTo={22} id="my-canvas" TD controls>
+  <Node />
+  <Node id="alpha" bgColor="blueviolet" label="Default Node" center />
+</Svelvet>
+</div>
